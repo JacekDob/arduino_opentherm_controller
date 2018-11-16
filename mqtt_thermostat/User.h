f0fa14d7874460664845be118f27edfc371ec7d6
repa6 <repayadam@gmail.com>
2018-11-16 10:19:16 +0100

    Create User.h

---
+++ mqtt_thermostat/User.h
@@ -0,0 +1,6 @@
+const char* ssid = "Please specify your WIFI SSID";
+const char* password = "Please specify your WIFI password";
+const char* mqtt_server = "Please specify MQTT server";
+const int   mqtt_port = 00000;
+const char* mqtt_user = "Please specify user";
+const char* mqtt_password = "Please specify password";
